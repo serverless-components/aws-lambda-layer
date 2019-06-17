718a9eda5181e36a7774e911e3dc6cf31e2d8ce0
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-06-17 14:31:51 +0300

    added packDir

--- utils.js
+++ utils.js
@@ -1,9 +1,63 @@
 const path = require('path')
 const { tmpdir } = require('os')
 const crypto = require('crypto')
-const { readFile } = require('fs-extra')
-const { equals, not, pick } = require('ramda')
+const archiver = require('archiver')
+const globby = require('globby')
+const { contains, isNil, last, split, equals, not, pick } = require('ramda')
+const { readFile, createReadStream, createWriteStream } = require('fs-extra')
 const { utils } = require('@serverless/core')
+
+const VALID_FORMATS = ['zip', 'tar']
+const isValidFormat = (format) => contains(format, VALID_FORMATS)
+
+const packDir = async (inputDirPath, outputFilePath, include = [], exclude = [], prefix) => {
+  const format = last(split('.', outputFilePath))
+
+  if (!isValidFormat(format)) {
+    throw new Error('Please provide a valid format. Either a "zip" or a "tar"')
+  }
+
+  const patterns = ['**']
+
+  if (!isNil(exclude)) {
+    exclude.forEach((excludedItem) => patterns.push(`!${excludedItem}`))
+  }
+
+  const files = (await globby(patterns, { cwd: inputDirPath }))
+    .sort() // we must sort to ensure correct hash
+    .map((file) => ({
+      input: path.join(inputDirPath, file),
+      output: prefix ? path.join(prefix, file) : file
+    }))
+
+  return new Promise((resolve, reject) => {
+    const output = createWriteStream(outputFilePath)
+    const archive = archiver(format, {
+      zlib: { level: 9 }
+    })
+
+    output.on('open', () => {
+      archive.pipe(output)
+
+      // we must set the date to ensure correct hash
+      files.forEach((file) =>
+        archive.append(createReadStream(file.input), { name: file.output, date: new Date(0) })
+      )
+
+      if (!isNil(include)) {
+        include.forEach((file) => {
+          const stream = createReadStream(file)
+          archive.append(stream, { name: path.basename(file), date: new Date(0) })
+        })
+      }
+
+      archive.finalize()
+    })
+
+    archive.on('error', (err) => reject(err))
+    output.on('close', () => resolve(outputFilePath))
+  })
+}
 
 const pack = async (code, prefix, include = []) => {
   if (utils.isArchivePath(code)) {
@@ -31,7 +85,7 @@ const pack = async (code, prefix, include = []) => {
   if (await utils.fileExists(outputFilePath)) {
     return outputFilePath
   }
-  return utils.packDir(code, outputFilePath, include, [], prefix)
+  return packDir(code, outputFilePath, include, [], prefix)
 }
 
 const publishLayer = async ({ lambda, name, description, runtimes, zipPath, bucket }) => {
@@ -104,6 +158,7 @@ const configChanged = (prevLayer = {}, layer) => {
 
 module.exports = {
   pack,
+  packDir,
   publishLayer,
   deleteLayer,
   getLayer,
